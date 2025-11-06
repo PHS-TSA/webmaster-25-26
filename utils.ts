23087aadb005797102a04c9c18406894fe87484a
Eli <88557639+lishaduck@users.noreply.github.com>
2025-11-06 14:18:20 -0600

    refactor: kill dead code cause yagni

--- utils.ts
+++ utils.ts
@@ -1,9 +1,10 @@
 import { createDefine } from "fresh";
 
-// This specifies the type of "ctx.state" which is used to share
-// data among middlewares, layouts and routes.
-export interface State {
-  shared: string;
-}
+/**
+ * This specifies the type of `ctx.state`,
+ * which is used to share data among middlewares, layouts and routes.
+ */
+// biome-ignore lint/suspicious/noEmptyInterface: Who even invented YAGNI?
+export interface State {}
 
 export const define = createDefine<State>();
